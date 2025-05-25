535414b2d237b6b9253a8f4b99545f57f9c1b201
Sierra555 <dara6234@gmail.com>
2025-05-25 20:53:03 +0300

    upd cors

--- src/server.ts
+++ src/server.ts
@@ -9,7 +9,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors());
+app.use(
+	cors({
+		origin: "https://chat-ai-ui-azure.vercel.app/",
+		credentials: true,
+	})
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
